a68ba792381657a3c75d7552a7f4bc411ad00e4e
Luke Boswell (Linux-Desktop) <lukewilliamboswell@gmail.com>
2026-08-21 18:43:50 +1000

    Highlight functions with trailing pipeline stages

--- queries-generated/zed/queries/highlights.scm
+++ queries-generated/zed/queries/highlights.scm
@@ -251,6 +251,11 @@
   (expr_body
     (function_call_pnc_expr
       caller: (anon_fun_expr))))
+(value_declaration (decl_left (identifier_pattern  (identifier) @function))
+  (expr_body
+    (bin_op_expr
+      part: (function_call_pnc_expr
+        caller: (anon_fun_expr)))))
 (function_call_pnc_expr caller: (variable_expr     (identifier) @function))
 (function_call_pnc_expr caller: (field_access_expr (identifier) @function .))
 (bin_op_expr (operator "->") (variable_expr        (identifier) @function))
